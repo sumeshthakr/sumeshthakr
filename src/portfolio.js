cd96733a05151e3e01b81b08af8bd1fa01b91433
Sumesh <sumesh.thakur@smu.ca>
2020-05-06 20:02:49 -0300

    thirs

---
+++ src/portfolio.js
@@ -0,0 +1,213 @@
+﻿
+/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  title: "Hi all my name is Sumesh Thakur",
+  subTitle: emoji("Graduate Student @ Graphics & Spatial Computing lab,SMU Halifax"),
+  resumeLink: ""
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+
+  github: "https://github.com/sumeshthakr",
+  linkedin: "https://www.linkedin.com/in/sumeshthkr/",
+  email: "sumeshthkr@gmail.com",
+  facebook: "https://www.facebook.com/sumesh.thakur.96"
+};
+
+// Your Skills Section
+
+const skillsSection = {
+  title: "What i do",
+  subTitle: "Computer Vision and Deep Learning Enthusiast",
+  skills: [
+    emoji("Currently working in the field of dynamic object detection and classification form point clouds"),
+    emoji("Proficient in modern computer vision algorithms, machine learning, deep learning, data analysis and android aplication development "),
+    emoji("Image processing in 3D , 3D reconstruction from point clouds")
+  ],
+
+/* Make Sure You include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "C++",
+      fontAwesomeClassname: "fab fa-cuttlefish"
+    },
+    {
+      skillName: "Python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "Java",
+      fontAwesomeClassname: "fab fa-java"
+    },
+    {
+      skillName: "Pytorch",
+      fontAwesomeClassname: "fab fa-facebook-f"
+    },
+    {
+      skillName: "Tensorflow",
+      fontAwesomeClassname: "fab fa-google"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "Vuejs",
+      fontAwesomeClassname: "fab fa-vuejs"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    
+ 
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Pytorch",  //Insert stack or technology you have experience in
+      progressPercentage: "90%"  //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Tensorflow",
+      progressPercentage: "60%"
+    },
+    {
+      Stack: "Procarstination",
+      progressPercentage: "10%"
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: "MzJhMjcwOTExZDQ2Y2ZiYmZmOWQxZTdmMzkyMzQ3Mzg4YmJiZDQ3MA==",
+  githubUserName: "sumesh.thakur@smu.ca",
+  showGithubProfile :"true" // Set true or false to show Contact profile using Github, defaults to false 
+};
+
+
+// Some Big Projects You have worked with your company
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "Coming SOON............",
+  /*projects: [
+    
+   {
+      image: "https://drive.google.com/uc?id=1MXoXcQRK-pH8J82wyjCW4SJk5AxJe7tf",
+      link: "http://nextu.se/"
+    }
+  ] */
+};
+
+// Your Achievement Section Include Your Certification Talks and More
+
+const achievementSection = {
+
+/*
+
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle: "Achievements, Certifications Award Letters and Some Cool Stuff that i have done !",
+
+  achivementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle: "First Pakistani to be selected as Google Google-in Finalist from 4000 students from 77 diffrent countries",
+      image:
+        "https://1.bp.blogspot.com/-Ig-v1tDXZt4/XDODmZvWp1I/AAAAAAAAB0A/KtbFdBPFVQw2O15FekkIR0Yg8MUp--rngCLcBGAs/s1600/GCI%2B-%2BVertical%2B-%2BGray%2BText%2B-%2BWhite%2BBG.png",
+      footerLink: [
+        { name: "Certification", url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing" },
+        { name: "Award Letter", url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing" },
+        { name: "Google Code-in Blog", url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html" }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Google_Assistant_logo.svg/1200px-Google_Assistant_logo.svg.png",
+      footerLink: [{ name: "View Google Assistant Action", url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en" }]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu93Bd6LDbxPEOXr-hfLqLYzmHny8c0MJoI3exQP-lwpFLRT7g&s",
+      footerLink: [
+        { name: "Certification", url: "" },
+        { name: "Final Project", url: "https://pakistan-olx-1.firebaseapp.com/" }
+      ]
+    }
+  ]
+  */
+};
+
+
+// Blogs Section
+
+const blogSection = {
+/*
+  title: "Blogs",
+  subtitle: "With Love for Developing cool stuff. i love to write and teach others what i have learned",
+
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description: "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min."
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT Is The Best?",
+      description: "React is a JavaScript library for building the user interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ]
+  */
+};
+
+
+
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle: "Discuss a project or just want to say hi my inbox is open for all",
+  number: "+1-7828820240",
+  email_address: "sumeshthkr@gmail.com"
+};
+
+const podcastSection = {
+
+/*
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: ["https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"]
+  */
+};
+
+//Twitter Section
+
+const twitterDetails = {
+
+  userName : "sumesh_thakur"//Replace "twitter" with your twitter username without @
+  
+};
+export { greeting, socialMediaLinks, skillsSection, techStack, openSource, bigProjects, achievementSection, blogSection, podcastSection, contactInfo , twitterDetails};
